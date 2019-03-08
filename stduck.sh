cefd84e05d83d36fe7e63288bb598cd88e400a92
kon <k@smo.la>
2019-03-08 15:10:56 +0000

    added right token

--- stduck.sh
+++ stduck.sh
@@ -1,1 +1,1 @@
-echo url="https://www.duckdns.org/update?domains=exampledomain&token=a7c4d0ad-114e-40ef-ba1d-d217904a50f2&ip=" | curl -k -o ~/duckdns/duck.log -K -
+echo url="https://www.duckdns.org/update?domains=srmedia&token=3d30c693-910c-4437-a289-4ed81b69ce7c&ip=" | curl -k -o ~/duckdns/duck.log -K -
